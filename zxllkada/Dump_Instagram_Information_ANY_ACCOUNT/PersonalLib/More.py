dba524241f5839d3fdea5caa8bf395f96a4318d5
kada <soulayman.kada@gmail.com>
2021-03-09 02:05:48 +0100

    Add files via upload

---
+++ zxllkada/Dump_Instagram_Information_ANY_ACCOUNT/PersonalLib/More.py
@@ -0,0 +1,23 @@
+#############################
+#############################
+
+Failed = "Something Worng"
+isData = 'window._sharedData = {"config":'
+Ghost1 = '<script type="text/javascript">window._sharedData = '
+Ghost2 = ';</script>'
+
+# Query
+QueryUrl = "https://www.instagram.com/graphql/query/"
+QueryHash= "?query_hash="
+HashContent = "d4d88dc1500312af6f937f7b804c68c3"
+QueryVariables = "&variables="
+FixParameter = "&page=2"
+QueryJson = {
+	"user_id":"",
+	"include_chaining":False,
+	"include_reel":False,
+	"include_suggested_users":False,
+	"include_logged_out_extras":True,
+	"include_highlight_reels":True,
+	"include_live_status":True
+}
