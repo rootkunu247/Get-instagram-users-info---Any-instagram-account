74fa317194c8bd89177b8f0a1c859cd9c260442a
kada <soulayman.kada@gmail.com>
2021-11-19 23:22:04 +0100

    Add files via upload

---
+++ zxllkada/Dump_Instagram_Information_ANY_ACCOUNT/SUPER_MODE/SuperLIB/screen/screen.py
@@ -0,0 +1,26 @@
+from SUPER_MODE.SuperLIB.screen.COLORS.colors import *
+import os, sys, json, time
+
+
+dict = json.load(open("SUPER_MODE/SuperLIB/screen/PACKAGES/screen.json"))
+os.system("clear")
+
+def Flush(Which, Color1, Color2, SleepTime1, SleepTime2):
+    for char in Which :
+        sys.stdout.write(Color1+char+Color2)
+        sys.stdout.flush()
+        time.sleep(SleepTime1)
+    time.sleep(SleepTime2)
+
+def Banner():
+    Flush("\n\n             "+dict["DRAWS"]["DRAW_1"]["STEP_1"], Y, W, 0, 0)
+    Flush("             "+dict["DRAWS"]["DRAW_1"]["STEP_2"], Y, W, 0, 0)
+    Flush("             "+dict["DRAWS"]["DRAW_1"]["STEP_3"], Y, W, 0, 0)
+    Flush("             "+dict["DRAWS"]["DRAW_1"]["STEP_4"]+"\n", Y, W, 0, 0)
+    Flush("     "+dict["DRAWS"]["DRAW_2"]["STEP_1"], G, W, 0.01, 0)
+    Flush("     "+dict["DRAWS"]["DRAW_2"]["STEP_2"], G, W, 0.01, 0)
+    Flush("     "+dict["DRAWS"]["DRAW_2"]["STEP_3"], G, W, 0.01, 0)
+    Flush("     "+dict["DRAWS"]["DRAW_2"]["STEP_4"], G, W, 0.01, 0)
+    Flush("     "+dict["DRAWS"]["DRAW_2"]["STEP_5"], G, W, 0.01, 0)
+    Flush("     Super Mode is running !\n", LB, W, 0.02, 0)
+
